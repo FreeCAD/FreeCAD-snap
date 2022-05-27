59e03bff4e767207129996067043e2bcf5135bcf
Maximilian Federle <max.federle@googlemail.com>
2022-05-27 21:00:36 +0200

    0.20beta1

--- snap/local/snap-setup-mod/Init.py
+++ snap/local/snap-setup-mod/Init.py
@@ -1,10 +1,19 @@
-import os
-import sys
+def add_snap_pythonpath():
+  import os
+  import sys
 
-pythonpath = os.environ.get("SNAP_PYTHONPATH")
+  pythonpath = os.environ.get("SNAP_PYTHONPATH")
+  if pythonpath:
+    print(f"Adding snap-specific PYTHONPATH to sys.path: {pythonpath}")
+    os.environ["PYTHONPATH"] = pythonpath
+    for path in pythonpath.split(":"):
+      sys.path.insert(0, path)
 
-if pythonpath:
-  print(f"Adding snap-specific PYTHONPATH to sys.path: {pythonpath}")
-  os.environ["PYTHONPATH"] = pythonpath
-  for path in pythonpath.split(":"):
-    sys.path.insert(0, path)
+def configure_mod_raytracing():
+  import FreeCAD
+  param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Raytracing")
+  if not param.GetString("PovrayExecutable", ""):
+    param.SetString("PovrayExecutable", "/snap/freecad/current/usr/bin/povray")
+
+add_snap_pythonpath()
+configure_mod_raytracing()
