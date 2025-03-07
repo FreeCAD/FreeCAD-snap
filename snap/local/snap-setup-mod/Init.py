87391402a42fd5aaa5399c176fcb3ae878d173da
Furgo <148809153+furgo16@users.noreply.github.com>
2025-03-07 14:24:20 +0100

    Add Wayland workaround

--- snap/local/snap-setup-mod/Init.py
+++ snap/local/snap-setup-mod/Init.py
@@ -21,6 +21,19 @@ def fix_theme():
   if param.GetBool("ThemeSearchPaths", False)  != param.GetBool("ThemeSearchPaths", True):
     param.SetBool("ThemeSearchPaths", False)
 
+def fix_wayland():
+  '''Temporary workaround to reset the QT_QPA_PLATFORM environment variable,
+  so that FreeCAD does not run on Wayland, which Coin3D does not yet support.
+  See known issues on https://github.com/FreeCAD/FreeCAD-snap/pull/179'''
+  import os
+
+  qtqpaplatformXcb = "xcb"
+  qtqpaplatform = os.environ.get("QT_QPA_PLATFORM")
+  if qtqpaplatform:
+    print(f"Resetting QT_QPA_PLATFORM environment variable from {qtqpaplatform} to '{qtqpaplatformXcb}'")
+    os.environ["QT_QPA_PLATFORM"] = qtqpaplatformXcb
+
 add_snap_pythonpath()
 configure_mod_mesh()
 fix_theme()
+fix_wayland()
