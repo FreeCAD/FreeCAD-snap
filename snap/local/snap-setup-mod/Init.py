75d9085456c53b9f9aa86085931079fcaebd6f00
Furgo <148809153+furgo16@users.noreply.github.com>
2025-11-18 16:07:53 +0100

    Upgrade FreeCAD snap to core24 and Qt 6 (#252)
    * Merge build and CI changes from master (#187)
    
    * Add PCL build dependency
    
    * Disable PCL support on build (#183)
    
    * Update VTK 7 => 9 (#184)
    
    * Build with container (#185)
    
    * Rework publish daily workflow (#186)
    
    - Bring back the log artifact upload
    - Use the snapcore action to do Snapcraft store uploads
    - Do not execute certain jobs if the build fails
    
    * Qt 6 build via kde-neon-6 (#179)
    
    * Update kde-neon extension
    
    Update to latest platform and build snap versions as described on https://snapcraft.io/docs/kde-neon-extension
    
    * Update to core 24
    * Update VTK 7 => 9
    * Upgrade libyaml package name for noble
    * Upgrade Python 3.10 => 3.12
    * Remove python3-numba, not compatible with Python 3.12
    * Disable all scikit-sparse dependencies. Some are not available in Python 3.12
    * Do not use python3-distutils with Python 3.12
    * Opencamlib not available in Python 3.12
    * Remove cleanup deletion and amd gpus id file
    * Bring back cleanup deletion code
    * Remove the X11 layout. It's added by kde-neon
    * Remove plugs already provided by the kde-neon extension
    * CMAKE_FIND_ROOT_PATH is already provided by kde-neon at build time
    * Assumes no longer required (and kde-neon already provides it)
    * Use snapcraft daily builds
    To be able to use fixes in https://github.com/canonical/snapcraft/pull/5261 before the Snapcraft 8.8.0 release (sceheduled in a few weeks time).
    * Add note about need to rename deps snap
    * Update reference to upstream deps snap
    * Update all relevant packages to Qt6
    * Update kde-neon extension to Qt6
    * Switch to Qt6 in the build
    * Disable non-working graphviz config generation for now
    * New way of not running on wayland
    * Workaround for missing symbol
    * Disable PCL support on build (#183)
    * Truly disable Wayland
    
    * Update badge link for Qt 6 daily publish workflow
    
    * Add Edge Qt 6 version badge to README
    
    Updated README to include Edge Qt 6 version badge and clarify edge builds.
    
    * Refactor layout for ElmerFEM and OpenMPI
    
    Updated layout and symlink configurations for ElmerFEM and OpenMPI in snapcraft.yaml.
    
    * Implement Elmer FEM solvers configuration
    
    Added a function to configure Elmer FEM solvers' paths.
    
    * Re-add opencamlib dependency to the core24 snap (#226)
    
    * Add python3-opencamlib to dependencies
    
    * Remove opencamlib from python-packages
    
    Remove commented out opencamlib package due to being unavailable for Python 3.12 in PyPi.
    
    * Upgrade Boost libraries to version 1.83.0
    
    * Add python3-vtk9 dependency for FEM workbench
    
    * Do not clean up shared memory libraries from the snap
    
    The kf6-core24 runtime snap provides its own copy of the OpenMPI libraries, unlike the kf5-core22 snap. The cleanup script saw these libraries in the kf6-core24 snap and removed them, which was incorrect. Add exclusion rules to protect these libraries from being removed.
    
    * Fix cleanup part syntax
    
    Inline comments broke the command.
    
    * Fix environment variable values in snapcraft.yaml to use string format
    
    * Introduce requirements.txt for Python package dependencies
    
    The goal is that dependabot can scan those dependencies by having them listed in the manifest
    
    * Add python3-lark as a build and runtime dependency for BIM Report command
    
    * Remove redundant packages already installed as a dependency
    
    * Disable Wayland in just one way
    
    Test as per: https://forum.snapcraft.io/t/kde-neon-6-extension-snap-qt-qpa-platform-overwritten/46054/8
    
    * Add dependabot for dependency scanning
    
    * Graphviz is already included in kf6-core24, remove it
    
    * Move requirements.txt manifest to correct location
    
    * Add plugins path for graphviz
    
    * Reintroduce graphviz part, now fixing the layout to make it work
    
    * Properly generate the missing graphviz config file
    
    Previous attempts failed at generating the config file. The current
    method of using GVBINDIR now succeeds.
    
    * Do not install ifcopenshell on arm64
    
    * Update workflows to publish the Qt6 build as default

--- snap/local/snap-setup-mod/Init.py
+++ snap/local/snap-setup-mod/Init.py
@@ -15,12 +15,43 @@ def configure_mod_mesh():
   if not param.GetString("gmshExe", ""):
     param.SetString("gmshExe", "/snap/freecad/current/usr/bin/gmsh")
 
+def configure_mod_fem_solvers():
+  """
+  Sets the default path for the Elmer FEM solvers if they are not already set.
+  This ensures FreeCAD's FEM workbench can find the Elmer binaries that are
+  bundled within the snap.
+  """
+  import FreeCAD
+  param_group = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Fem/Elmer")
+
+  # --- Configure ElmerSolver ---
+  if not param_group.GetString("elmerBinaryPath", ""):
+    param_group.SetString("elmerBinaryPath", "/snap/freecad/current/usr/bin/ElmerSolver")
+
+  # --- Configure ElmerGrid ---
+  if not param_group.GetString("gridBinaryPath", ""):
+    param_group.SetString("gridBinaryPath", "/snap/freecad/current/usr/bin/ElmerGrid")
+
 def fix_theme():
   import FreeCAD
   param = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Bitmaps/Theme")
   if param.GetBool("ThemeSearchPaths", False)  != param.GetBool("ThemeSearchPaths", True):
     param.SetBool("ThemeSearchPaths", False)
 
+def fix_wayland():
+  '''Temporary workaround to reset the QT_QPA_PLATFORM environment variable,
+  so that FreeCAD does not run on Wayland, which Coin3D does not yet support.
+  See known issues on https://github.com/FreeCAD/FreeCAD-snap/pull/179'''
+  import os
+
+  qtqpaplatformXcb = "xcb"
+  qtqpaplatform = os.environ.get("QT_QPA_PLATFORM")
+  if qtqpaplatform:
+    print(f"Resetting QT_QPA_PLATFORM environment variable from {qtqpaplatform} to '{qtqpaplatformXcb}'")
+    os.environ["QT_QPA_PLATFORM"] = qtqpaplatformXcb
+
 add_snap_pythonpath()
 configure_mod_mesh()
+configure_mod_fem_solvers()
 fix_theme()
+# fix_wayland()
